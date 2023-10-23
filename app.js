7c675742dfe60b78dc3720956ec5329f2b7fc757
derek <szhtike004@gmail.com>
2023-10-23 22:29:33 +0630

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+// Require .env as config
+require(`dotenv`).config()
+
+const express = require(`express`)
+const expresslayout = require(`express-ejs-layouts`)
+
+const app = express()
+// || Port for production in server
+const PORT = 8000 || process.env.PORT
+
+// Public folder for js, css, images
+app.use(express.static(`public`))
+
+// Templating engine in middleware
+app.use(expresslayout)
+app.set(`layout`, `./layouts/main`)
+app.set(`view engine`, `ejs`)
+
+// For routes components
+app.use(`/`, require(`./server/routes/main`))
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`)
+})
+
