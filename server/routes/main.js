ba8be5c02812abe1f9fc5f53a9c63b45a5029307
derek <szhtike004@gmail.com>
2023-10-24 11:26:14 +0630

    [add]header and footer layout

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,21 @@ const router = express.Router()
 
 // Routes
 router.get(``, (req, res) => {
-    res.send(`Hello World`)
+    const locals = {
+        title: `Node JS Blog`,
+        desc: `Simple blog with node, express and mongodb`
+    }
+
+
+    res.render(`index`, { locals })
+})
+
+router.get(`/about`, (req, res) => {
+    res.render(`about`)
+})
+
+router.get(`/contact`, (req,res) => {
+    res.render(`contact`)
 })
 
 module.exports = router
