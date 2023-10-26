a5fcf79932d74dc0869b02985c575e41a337e47f
derek <szhtike004@gmail.com>
2023-10-26 15:26:35 +0630

    [add] connection with cloud mongodb

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ require(`dotenv`).config()
 const express = require(`express`)
 const expresslayout = require(`express-ejs-layouts`)
 
+const connectDB = require(`./server/config/db`)
+
 const app = express()
 // || Port for production in server
 const PORT = 8000 || process.env.PORT
+
+// Connect to MongoDB
+connectDB()
 
 // Public folder for js, css, images
 app.use(express.static(`public`))
