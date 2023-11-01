bdbab5a46c80167e43598ab1fa06e658aca055d7
derek <szhtike004@gmail.com>
2023-11-01 10:41:57 +0630

    [add] pagination and fix buffering 10000ms mongodb error

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,23 +1,69 @@
 const express = require(`express`)
 const router = express.Router()
+const Post = require("../models/Post")
 
-// Routes
-router.get(``, (req, res) => {
-    const locals = {
-        title: `Node JS Blog`,
-        desc: `Simple blog with node, express and mongodb`
+
+/**
+ * GET/
+ * Home
+ */
+router.get(``, async (req, res) => {
+    try {
+        const perPage = 5;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .exec();
+
+        const count = await Post.count();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage)
+
+        res.render(`index`, {
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null,
+            previousPage : page -1
+        });
+
+    } catch (error) {
+        console.log(`ERROR : ${error}`)
     }
+});
 
 
-    res.render(`index`, { locals })
-})
+
+
+
+
+
+
+
 
 router.get(`/about`, (req, res) => {
     res.render(`about`)
 })
 
-router.get(`/contact`, (req,res) => {
+router.get(`/contact`, (req, res) => {
     res.render(`contact`)
 })
+
+
+// function insertPostData() {
+//     post.insertMany([
+//         {
+//             title: "This is title",
+//             body: "Building blog with NodeJs"
+//         },
+//         {
+//             title: "This is title2",
+//             body: "Building blog with NodeJs 2"
+//         },
+//     ])
+// }
+
+// insertPostData()
 
 module.exports = router
