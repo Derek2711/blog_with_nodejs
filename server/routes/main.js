8724075c1fa2032cc6d4c6c8a029780ccd4e79ec
derek <szhtike004@gmail.com>
2023-11-03 10:08:58 +0630

    [change] to local mongobo from atlas mongo

--- server/routes/main.js
+++ server/routes/main.js
@@ -9,6 +9,11 @@ const Post = require("../models/Post")
  */
 router.get(``, async (req, res) => {
     try {
+        const local = {
+            title: `Personal blog with Nodejs`,
+            desc: `Building a blog with Node, Express and MongoDB`
+        }
+
         const perPage = 5;
         let page = req.query.page || 1;
 
@@ -22,24 +27,17 @@ router.get(``, async (req, res) => {
         const hasNextPage = nextPage <= Math.ceil(count / perPage)
 
         res.render(`index`, {
+            local,
             data,
             current: page,
             nextPage: hasNextPage ? nextPage : null,
-            previousPage : page -1
+            previousPage: page - 1
         });
 
     } catch (error) {
         console.log(`ERROR : ${error}`)
     }
 });
-
-
-
-
-
-
-
-
 
 
 router.get(`/about`, (req, res) => {
@@ -50,9 +48,14 @@ router.get(`/contact`, (req, res) => {
     res.render(`contact`)
 })
 
+router.use((req, res) => {
+    res.status(400).render(`404`)
+})
+
+module.exports = router
 
 // function insertPostData() {
-//     post.insertMany([
+//     Post.insertMany([
 //         {
 //             title: "This is title",
 //             body: "Building blog with NodeJs"
@@ -61,9 +64,48 @@ router.get(`/contact`, (req, res) => {
 //             title: "This is title2",
 //             body: "Building blog with NodeJs 2"
 //         },
+//         {
+//             title: "This is title 3",
+//             body: "Building blog with NodeJs 3"
+//         },
+//         {
+//             title: "This is title 4",
+//             body: "Building blog with NodeJs 4"
+//         },
+//         {
+//             title: "This is title 5",
+//             body: "Building blog with NodeJs 5"
+//         },
+//         {
+//             title: "This is title 6",
+//             body: "Building blog with NodeJs 6"
+//         },
+//         {
+//             title: "This is title 7",
+//             body: "Building blog with NodeJs 7"
+//         },
+//         {
+//             title: "This is title 8",
+//             body: "Building blog with NodeJs 8"
+//         },
+//         {
+//             title: "This is title 9",
+//             body: "Building blog with NodeJs 9"
+//         },
+//         {
+//             title: "This is title 10",
+//             body: "Building blog with NodeJs 10"
+//         },
+//         {
+//             title: "This is title 11",
+//             body: "Building blog with NodeJs 11"
+//         },
+//         {
+//             title: "This is title 12",
+//             body: "Building blog with NodeJs 12"
+//         },
 //     ])
 // }
 
 // insertPostData()
 
-module.exports = router
